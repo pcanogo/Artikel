cc95c8bccea1538b3d707b83a222846b8328a970
Pablo  Cano <pablocano@Pablos-MacBook-Pro.local>
2020-06-13 00:20:42 -0500

    word_image_service promise updates

--- services/word-images/word_image_service.ts
+++ services/word-images/word_image_service.ts
@@ -2,16 +2,16 @@ import { WordImage } from '../../types.ts'
 
 export abstract class WordImageService {
 
-    abstract getWordImage(id: string) : WordImage | undefined
-    abstract createWordImage(newWordImage: WordImage) : string
-    abstract editWordImage(id: string, updatedWordImage: WordImage) : string
-    abstract deleteWordImage(id: string) : void
+    abstract getWordImage(id: string) : WordImage | undefined | Promise<WordImage | undefined>
+    abstract createWordImage(newWordImage: WordImage) : string | Promise<string>
+    abstract editWordImage(id: string, updatedWordImage: WordImage) : string | Promise<string>
+    abstract deleteWordImage(id: string) : void | Promise<void>
 
-    abstract getAllWordImages() : WordImage[]
-    abstract getItemImages(itemId: string) : WordImage[] 
-    abstract getItemImagesURL(itemId: string) : string[]
-    abstract deleteItemImages(itemId: string) : void
+    abstract getAllWordImages() : WordImage[] | Promise<WordImage[]>
+    abstract getItemImages(itemId: string) : WordImage[] | Promise<WordImage[]>
+    abstract getItemImagesURL(itemId: string) : string[] | Promise<string[]>
+    abstract deleteItemImages(itemId: string) : void | Promise<void>
 
     abstract getUserItemImages(userId: string) : WordImage[] | Promise<WordImage[]>
-    abstract deleteUserItemImages(userId: string) : void
+    abstract deleteUserItemImages(userId: string) : void | Promise<void>
 }
